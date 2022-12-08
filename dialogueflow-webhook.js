7ca021c84d8ba75416d11420a1fe98afc0413736
K2Lin <77517478+K2Lin-Daniel@users.noreply.github.com>
2022-12-08 04:36:53 -0800

    Update dialogueflow-webhook.js

--- dialogueflow-webhook.js
+++ dialogueflow-webhook.js
@@ -18,33 +18,32 @@ app.post("/dialogflow", express.json(), (req, res) => {
   async function queryGPT(agent) {
       // agent.add('Sorry! I am unable to understand this at the moment. I am still learning humans. You can pick any of the service that might help me.');
       const instance = axios.create({
-        baseURL: 'https://api.openai.com/v1/completions',
+        baseURL: 'https://api.openai.com/v1/',
         headers: { Authorization: `Bearer ${process.env.OPENAI_API_KEY}` },
       });
     
       const dialog = [
-        ``,
+        `Hi! Kyrin chat AI Gen2 desu!`,
       ];
       let query = agent.query;
       console.log('querytext ', query)
       dialog.push(`User: ${query}`);
-      dialog.push('Kyrin:');
+      dialog.push('AI:');
       // agent.add(`you said ${query}`)
     
       const completionParmas = {
         prompt: dialog.join('\n'),
-        model: "davinci:ft-yajusenpai-red-tea-co-ltd:kyrin-chan-ai-gen2-2022-12-08-04-19-23",
         max_tokens: 60,
         temperature: 0.85,
         n: 1,
         stream: false,
         logprobs: null,
         echo: false,
-        stop: 'END',
+        stop: '\n',
       };
     
       try {
-        const result = await instance.post('', completionParmas);
+        const result = await instance.post('/engines/davinci/completions', completionParmas);
         const botResponse = result.data.choices[0].text.trim();
         agent.add(botResponse);
       } catch (err) {
@@ -54,5 +53,5 @@ app.post("/dialogflow", express.json(), (req, res) => {
     
   }
 });
-const port = 24971;
+const port = 3000;
 app.listen(port, () => console.log(`App listening on port ${port}!`))
